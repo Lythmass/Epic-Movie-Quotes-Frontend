d19c8af1a5883aad21deab8e141da0021b4911c1
Lythmass <lythmass17@gmail.com>
2023-01-06 18:51:23 +0400

    chore: move images data to another folder

---
+++ images-data/index.ts
@@ -0,0 +1,1 @@
+export { default as imagesData } from './ImagesData';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { NavBar, Button, LandingImages } from 'components';
-
+import { imagesData } from 'images-data';
 export default function Home() {
-  const imagesData = [
-    {
-      name: 'interstellar',
-      title: 'Interstellar, 2014',
-      quote: 'You have to leave something behind to go forward',
-    },
-    {
-      name: 'tenenbaums',
-      title: 'The Royal Tenenbaums,2001',
-      quote: `I think we’re just gonna have to be secretly in love with each other and leave it that`,
-    },
-    {
-      name: 'tlotr',
-      title: 'The Lord of the Rings, 2003',
-      quote:
-        'I see in your eyes the same fear that would take the heart of me...',
-    },
-  ];
   return (
     <>
       <section className='w-full min-h-screen flex flex-col justify-start items-center bg-dark-bg pt-6'>
