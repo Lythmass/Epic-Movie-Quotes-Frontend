a7ec3165a07297882231dcde87bf948bbfc33d4e
Lythmass <lythmass17@gmail.com>
2023-01-09 13:00:58 +0400

    fix: images not loading correctly

--- components/LandingPageComponents/LandingImage/LandingImage.tsx
+++ components/LandingPageComponents/LandingImage/LandingImage.tsx
@@ -5,7 +5,8 @@ export default function LandingImage(props: LandingImageType) {
   return (
     <div className='w-full h-[27rem]'>
       <div
-        className={`bg-[url('/assets/images/${props.src}.png')] bg-fixed bg-cover bg-center w-full h-full flex flex-col justify-center items-center`}
+        style={{ backgroundImage: `url('/assets/images/${props.src}.png')` }}
+        className='bg-fixed bg-cover bg-center w-full h-full flex flex-col justify-center items-center'
       >
         <div className='w-[80%] flex flex-col gap-2 justify-center items-start'>
           <h1 className='relative text-left text-white text-xl font-bold'>
