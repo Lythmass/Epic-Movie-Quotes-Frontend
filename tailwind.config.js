9dc0c64ba87225c2c945a2c3a4d63305aadef391
Lythmass <lythmass17@gmail.com>
2023-01-10 16:18:02 +0400

    style: add registration modal for desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,13 @@ module.exports = {
       colors: {
         'skin-color': '#DDCCAA',
         'button-red': '#E31221',
+        'frozen-bg': 'rgba(0, 0, 0, 0.54)',
       },
       backgroundImage: {
         'dark-bg':
           'linear-gradient(180deg, #11101A 0%, #08080D 50.52%, rgba(0, 0, 0, 0) 100%)',
         'modal-bg':
-          'linear-gradient(187.16deg, #181623 0.07%, #191725 51.65%, #0D0B14 98.75%);',
+          'linear-gradient(187.16deg, #181623 0.07%, #191725 51.65%, #0D0B14 98.75%)',
       },
     },
   },
