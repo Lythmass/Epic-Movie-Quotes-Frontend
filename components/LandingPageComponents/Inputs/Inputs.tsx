2159477e82cd14095a71a53d7771cc5248872330
Lythmass <lythmass17@gmail.com>
2023-01-16 13:56:37 +0400

    feat(register): send data to laravel api
    refactor code and move some logic to helpers and custom hooks, update language changer paths, install axios and add routes to api, then send registration data to laravel api, manually setErrors in react hook forms if the server returns errors. verify email using the verification url sent back by the server.

--- components/LandingPageComponents/Inputs/Inputs.tsx
+++ components/LandingPageComponents/Inputs/Inputs.tsx
@@ -1,6 +1,6 @@
 import { InputsType } from 'types';
 import { useFormContext } from 'react-hook-form';
-import { validation } from 'helpers';
+import { validation, registerParameters } from 'helpers';
 import { usePasswordConfirmation } from 'hooks';
 import { useTranslation } from 'react-i18next';
 
@@ -8,6 +8,14 @@ export default function InputLayout(props: InputsType) {
   const methods: any = useFormContext();
   const validatePasswordConfirmation = usePasswordConfirmation(methods);
   const { t } = useTranslation();
+
+  const [registerName, validationRule] = registerParameters(
+    t,
+    props,
+    validation,
+    validatePasswordConfirmation
+  );
+
   return (
     <div className='flex flex-col w-full'>
       <label className='text-white' htmlFor={props.label}>
@@ -15,20 +23,14 @@ export default function InputLayout(props: InputsType) {
         <span className='text-button-red'> {!props.isOptional && '*'}</span>
       </label>
       <input
-        {...methods.register(
-          props.label.toLowerCase(),
-          props.label.toLowerCase() ===
-            t('registrationModal.confirm-password').toLowerCase()
-            ? validatePasswordConfirmation
-            : validation[t(props.validationIndex, { lng: 'en' }).toLowerCase()]
-        )}
+        {...methods.register(registerName, validationRule)}
         autoComplete='off'
         className='py-2 px-3 rounded-[0.25rem] bg-[#CED4DA] placeholder-[#6C757D]'
         type={props.type}
         placeholder={props.placeholder}
       />
       <p className='text-button-red h-4 pt-1'>
-        {t(methods.formState.errors[props.label.toLowerCase()]?.message)}
+        {t(methods.formState.errors[registerName]?.message)}
       </p>
     </div>
   );
