c11f007957c25ab11bc73405c6d7dd062965d5b2
Lythmass <lythmass17@gmail.com>
2023-01-18 12:07:39 +0400

    feat: save locale in cookie
    save locales in cookies-next

--- helpers/languageChangerHelpers.ts
+++ helpers/languageChangerHelpers.ts
@@ -1,6 +1,10 @@
+import { setCookie } from 'cookies-next';
 export default function clickHandler(
   language: string,
   setCurrentLanguage: (value: string) => void
 ) {
   setCurrentLanguage(language);
+  setCookie('locale', language === 'Eng' ? 'en' : 'ka', {
+    sameSite: true,
+  });
 }
--- hooks/useChangeLanguage.ts
+++ hooks/useChangeLanguage.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { UseChangeLanguageType } from 'types';
 import { useTranslation } from 'react-i18next';
-
+import { getCookie } from 'cookies-next';
 export default function useChangeLanguage(): UseChangeLanguageType {
   const [currentLanguage, setCurrentLanguage] = useState('Eng');
   const [optionsModal, setOptionsModal] = useState(false);
@@ -10,10 +10,12 @@ export default function useChangeLanguage(): UseChangeLanguageType {
   useEffect(() => {
     i18n.changeLanguage(currentLanguage === 'Eng' ? 'en' : 'ka');
   }, [i18n, currentLanguage]);
+  useEffect(() => {
+    setCurrentLanguage(getCookie('locale') === 'ka' ? 'Geo' : 'Eng');
+  }, []);
 
   function clickHandler() {
     setOptionsModal((oldValue) => !oldValue);
   }
-
   return [currentLanguage, setCurrentLanguage, optionsModal, clickHandler];
 }
