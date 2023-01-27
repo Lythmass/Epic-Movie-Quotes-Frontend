4942ae2a08917aa19964fa75d018fa4b9e47a669
Lythmass <lythmass17@gmail.com>
2023-01-27 14:31:21 +0400

    feat: add localization to profile page
    translate everything

--- components/ProfilePageComponents/DesktopInputsBlock/DesktopInputsBlock.tsx
+++ components/ProfilePageComponents/DesktopInputsBlock/DesktopInputsBlock.tsx
@@ -1,29 +1,33 @@
 import { ProfileInputsForDesktop, AddEmailButton } from 'components';
 import { useWindowWidth } from 'hooks';
-
+import { useTranslation } from 'next-i18next';
 export default function DesktopInputsBlock() {
   const screenWidth = useWindowWidth();
+  const { t } = useTranslation('profile');
   return (
     <div className='mt-20 w-full'>
       <ProfileInputsForDesktop
-        placeholder='Enter your username'
-        label='Username'
+        placeholder={t('enter', { name: t('username') })}
+        label={t('username')}
         type='text'
+        name='username'
       />
       <hr className='border-[#CED4DA80] opacity-50 w-[72%] my-8' />
       <div className='flex flex-col gap-8'>
         <ProfileInputsForDesktop
-          placeholder='Enter your email'
-          label='Email'
+          placeholder={t('enter', { name: t('email') })}
+          label={t('email')}
           type='mail'
+          name='email'
         />
         {screenWidth > 1024 && <AddEmailButton />}
       </div>
       <hr className='border-[#CED4DA80] opacity-50 w-[72%] my-8' />
       <ProfileInputsForDesktop
-        placeholder='Enter your password'
-        label='Password'
+        placeholder={t('enter', { name: t('password') })}
+        label={t('password')}
         type='password'
+        name='password'
       />
     </div>
   );
--- components/ProfilePageComponents/MobileEmailButton/MobileEmailButton.tsx
+++ components/ProfilePageComponents/MobileEmailButton/MobileEmailButton.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
+import { useTranslation } from 'next-i18next';
 export default function MobileEmailButton() {
+  const { t, i18n } = useTranslation('profile');
   return (
     <div className='flex justify-between items-center w-[80%]'>
-      <p>EMAIL</p>
+      <p>{i18n.language === 'en' ? t('email').toUpperCase() : t('email')}</p>
       <img src='/assets/images/front-arrow.png' alt='change email' />
     </div>
   );
