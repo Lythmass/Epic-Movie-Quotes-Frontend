846ff0fd6a452abd8f41ce11311c2f2420c06168
Lythmass <lythmass17@gmail.com>
2023-01-27 16:23:57 +0400

    chore: use css uppercase

--- components/ProfilePageComponents/MobileEmailButton/MobileEmailButton.tsx
+++ components/ProfilePageComponents/MobileEmailButton/MobileEmailButton.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 import { useTranslation } from 'next-i18next';
 export default function MobileEmailButton() {
-  const { t, i18n } = useTranslation('profile');
+  const { t } = useTranslation('profile');
   return (
     <div className='flex justify-between items-center w-[80%]'>
-      <p>{i18n.language === 'en' ? t('email').toUpperCase() : t('email')}</p>
+      <p className='uppercase'>{t('email')}</p>
       <img src='/assets/images/front-arrow.png' alt='change email' />
     </div>
   );
