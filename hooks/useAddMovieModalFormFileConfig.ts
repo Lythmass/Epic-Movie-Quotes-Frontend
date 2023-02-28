280ec2182c1d661383c134294832fd90cb36a715
Lythmass <lythmass17@gmail.com>
2023-02-28 16:11:08 +0400

    feat: show image preview
    closes #91

--- hooks/useAddMovieModalFormFileConfig.ts
+++ hooks/useAddMovieModalFormFileConfig.ts
@@ -1,12 +1,13 @@
 import { useWindowWidth } from 'hooks';
 import { useTranslation } from 'next-i18next';
-import { useRef, DragEvent, useEffect } from 'react';
+import { useRef, DragEvent, useEffect, useState } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
 
 export default function useAddMovieModalFormFileConfig() {
   const methods = useFormContext<{ thumbnail: FileList }>();
   const { t } = useTranslation('movies');
   const imageRef = useRef<HTMLInputElement>(null);
+  const [image, setImage] = useState('');
   const dropHandler = (event: DragEvent<HTMLDivElement>) => {
     event.preventDefault();
     imageRef.current!.files = event.dataTransfer.files;
@@ -18,11 +19,18 @@ export default function useAddMovieModalFormFileConfig() {
   });
   useEffect(() => {
     const emptyFileList = new DataTransfer().files;
+    if (
+      (imageRef.current!.files != null ||
+        imageRef.current!.files != undefined) &&
+      imageRef.current!.files[0] != undefined
+    ) {
+      setImage(URL.createObjectURL(imageRef.current!.files[0]));
+    }
     methods.setValue(
       'thumbnail',
       imageRef.current!.files != null ? imageRef.current!.files : emptyFileList
     );
   }, [watchFile, methods]);
   const windowSize = useWindowWidth();
-  return { methods, t, imageRef, dropHandler, windowSize };
+  return { methods, t, imageRef, dropHandler, windowSize, image };
 }
